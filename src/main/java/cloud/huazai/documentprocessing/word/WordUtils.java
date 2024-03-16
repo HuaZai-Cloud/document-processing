ea65b455578be4b446e094492b1f08ff7054e53b
HuaZai <huazai@huazai.cloud>
2024-03-16 19:54:18 +0800

    document processing -> Word add picture

--- src/main/java/cloud/huazai/documentprocessing/word/WordUtils.java
+++ src/main/java/cloud/huazai/documentprocessing/word/WordUtils.java
@@ -1,14 +1,15 @@
 package cloud.huazai.documentprocessing.word;
 
+import org.apache.poi.common.usermodel.PictureType;
+import org.apache.poi.poifs.filesystem.FileMagic;
 import org.apache.poi.util.Units;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRelation;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.*;
+import org.openxmlformats.schemas.drawingml.x2006.main.CTHyperlink;
 import org.openxmlformats.schemas.drawingml.x2006.main.CTNonVisualDrawingProps;
 import org.openxmlformats.schemas.drawingml.x2006.wordprocessingDrawing.CTInline;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTDrawing;
 
+import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
@@ -21,139 +22,72 @@ import java.net.URLConnection;
  */
 public class WordUtils {
 
-
-	public static void demo(XWPFDocument document,String imgUrl){
-
-		XWPFParagraph paragraph = document.createParagraph();
-		XWPFRun run = paragraph.createRun();
-
-		int format;
-		if (imgUrl.endsWith(".emf")) {
-			format = XWPFDocument.PICTURE_TYPE_EMF;
-		} else if (imgUrl.endsWith(".wmf")) {
-			format = XWPFDocument.PICTURE_TYPE_WMF;
-		} else if (imgUrl.endsWith(".pict")) {
-			format = XWPFDocument.PICTURE_TYPE_PICT;
-		} else if (imgUrl.endsWith(".jpeg") || imgUrl.endsWith(".jpg")) {
-			format = XWPFDocument.PICTURE_TYPE_JPEG;
-		} else if (imgUrl.endsWith(".png")) {
-			format = XWPFDocument.PICTURE_TYPE_PNG;
-		} else if (imgUrl.endsWith(".dib")) {
-			format = XWPFDocument.PICTURE_TYPE_DIB;
-		} else if (imgUrl.endsWith(".gif")) {
-			format = XWPFDocument.PICTURE_TYPE_GIF;
-		} else if (imgUrl.endsWith(".tiff")) {
-			format = XWPFDocument.PICTURE_TYPE_TIFF;
-		} else if (imgUrl.endsWith(".eps")) {
-			format = XWPFDocument.PICTURE_TYPE_EPS;
-		} else if (imgUrl.endsWith(".bmp")) {
-			format = XWPFDocument.PICTURE_TYPE_BMP;
-		} else if (imgUrl.endsWith(".wpg")) {
-			format = XWPFDocument.PICTURE_TYPE_WPG;
-		} else {
-			return;
-		}
-		int index=0;
+	public static void insertPicture(XWPFDocument document,String pictureUrl,double width, double height ,boolean isAddConnection,ParagraphAlignment alignment){
 		try {
-			URL	url = new URL(imgUrl);
+			URL	url = new URL(pictureUrl);
 			URLConnection urlConn = url.openConnection();
 			InputStream inputStream = urlConn.getInputStream();
-
-
-			if (null != inputStream) {
-				String relationshipId = paragraph.getDocument().getPackagePart()
-						.addExternalRelationship(imgUrl, XWPFRelation.HYPERLINK.getRelation()).getId();
-
-				run.addPicture(inputStream, format,"图片", Units.toEMU(440), Units.toEMU(230));
-
-				if (run.getCTR().getDrawingList() != null && !run.getCTR().getDrawingList().isEmpty()) {
-					CTDrawing ctDrawing = run.getCTR().getDrawingList().get(index);
-					if (ctDrawing.getInlineList() != null && !ctDrawing.getInlineList().isEmpty()) {
-						CTInline ctInline = ctDrawing.getInlineList().get(0);
-						CTNonVisualDrawingProps docPr = ctInline.getDocPr();
-						if (docPr != null) {
-							org.openxmlformats.schemas.drawingml.x2006.main.CTHyperlink hlinkClick = docPr.addNewHlinkClick();
-							hlinkClick.setId(relationshipId);
-						}
-					}
-					index++;
-				}
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-
-
-
-
-	}
-
-	public static void demo2(XWPFDocument document,String imgUrl){
-
-
-		int format;
-		if (imgUrl.endsWith(".emf")) {
-			format = XWPFDocument.PICTURE_TYPE_EMF;
-		} else if (imgUrl.endsWith(".wmf")) {
-			format = XWPFDocument.PICTURE_TYPE_WMF;
-		} else if (imgUrl.endsWith(".pict")) {
-			format = XWPFDocument.PICTURE_TYPE_PICT;
-		} else if (imgUrl.endsWith(".jpeg") || imgUrl.endsWith(".jpg")) {
-			format = XWPFDocument.PICTURE_TYPE_JPEG;
-		} else if (imgUrl.endsWith(".png")) {
-			format = XWPFDocument.PICTURE_TYPE_PNG;
-		} else if (imgUrl.endsWith(".dib")) {
-			format = XWPFDocument.PICTURE_TYPE_DIB;
-		} else if (imgUrl.endsWith(".gif")) {
-			format = XWPFDocument.PICTURE_TYPE_GIF;
-		} else if (imgUrl.endsWith(".tiff")) {
-			format = XWPFDocument.PICTURE_TYPE_TIFF;
-		} else if (imgUrl.endsWith(".eps")) {
-			format = XWPFDocument.PICTURE_TYPE_EPS;
-		} else if (imgUrl.endsWith(".bmp")) {
-			format = XWPFDocument.PICTURE_TYPE_BMP;
-		} else if (imgUrl.endsWith(".wpg")) {
-			format = XWPFDocument.PICTURE_TYPE_WPG;
-		} else {
-			return;
-		}
-		int index=0;
-		try {
-			URL	url = new URL(imgUrl);
-			URLConnection urlConn = url.openConnection();
-			InputStream inputStream = urlConn.getInputStream();
-			String id = document.addPictureData(inputStream, format);
 
 			XWPFParagraph paragraph = document.createParagraph();
 			XWPFRun run = paragraph.createRun();
 
-
 			if (null != inputStream) {
 
-				// String relationshipId = paragraph.getDocument().getPackagePart()
-				// 		.addExternalRelationship(imgUrl, XWPFRelation.HYPERLINK.getRelation()).getId();
+				BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
 
-				run.addPicture(inputStream, format,"图片", Units.toEMU(440), Units.toEMU(230));
+				PictureType pictureType = PictureType.valueOf(FileMagic.valueOf(bufferedInputStream));
+				int format = pictureType.getOoxmlId();
 
-				if (run.getCTR().getDrawingList() != null && !run.getCTR().getDrawingList().isEmpty()) {
-					CTDrawing ctDrawing = run.getCTR().getDrawingList().get(index);
-					if (ctDrawing.getInlineList() != null && !ctDrawing.getInlineList().isEmpty()) {
-						CTInline ctInline = ctDrawing.getInlineList().get(0);
-						CTNonVisualDrawingProps docPr = ctInline.getDocPr();
-						if (docPr != null) {
-							org.openxmlformats.schemas.drawingml.x2006.main.CTHyperlink hlinkClick = docPr.addNewHlinkClick();
-							hlinkClick.setId(id);
+				paragraph.setAlignment(alignment);
+
+				run.addPicture(bufferedInputStream, format,"picture", (int) Math.rint(width * Units.EMU_PER_CENTIMETER),(int)Math.rint(height*Units.EMU_PER_CENTIMETER));
+
+				if (isAddConnection) {
+
+					String relationshipId = document.getPackagePart().addExternalRelationship(pictureUrl, XWPFRelation.HYPERLINK.getRelation()).getId();
+
+					if (run.getCTR().getDrawingList() != null && !run.getCTR().getDrawingList().isEmpty()) {
+						CTDrawing ctDrawing = run.getCTR().getDrawingList().get(0);
+						if (ctDrawing.getInlineList() != null && !ctDrawing.getInlineList().isEmpty()) {
+							CTInline ctInline = ctDrawing.getInlineList().get(0);
+							CTNonVisualDrawingProps docPr = ctInline.getDocPr();
+							if (docPr != null) {
+								CTHyperlink linkClick = docPr.addNewHlinkClick();
+								linkClick.setId(relationshipId);
+							}
 						}
 					}
-					index++;
 				}
 			}
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-
-
 	}
+
+
+
+	public static void insertPicture(XWPFDocument document,InputStream pictureInputStream,double width, double height ,ParagraphAlignment alignment){
+
+		try {
+
+			XWPFParagraph paragraph = document.createParagraph();
+			XWPFRun run = paragraph.createRun();
+
+			if (null != pictureInputStream) {
+
+				BufferedInputStream bufferedInputStream = new BufferedInputStream(pictureInputStream);
+
+				PictureType pictureType = PictureType.valueOf(FileMagic.valueOf(bufferedInputStream));
+				int format = pictureType.getOoxmlId();
+				paragraph.setAlignment(alignment);
+
+				run.addPicture(bufferedInputStream, format,"picture", (int) Math.rint(width * Units.EMU_PER_CENTIMETER),(int)Math.rint(height*Units.EMU_PER_CENTIMETER));
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 
 
 }
--- src/test/java/cloud/huazai/documentprocessing/word/WordUtilsTest.java
+++ src/test/java/cloud/huazai/documentprocessing/word/WordUtilsTest.java
@@ -1,11 +1,15 @@
 package cloud.huazai.documentprocessing.word;
 
 
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * WordUtilsTest
@@ -16,22 +20,51 @@ import java.io.IOException;
 public class WordUtilsTest {
 
 	@Test
-	public void demo() {
+	public void insertPictureTest() {
 
 
 		XWPFDocument document = new XWPFDocument();
 
 		String imgUrl = "https://mhp-test.oss-cn-qingdao.aliyuncs.com/images/test/ms/file/notFilename/20230920161635/20230920161634_3_.png";
 
-		WordUtils.demo2(document, imgUrl);
+		WordUtils.insertPicture(document, imgUrl,10,12,true, ParagraphAlignment.CENTER);
 
-		String outPath = "/Users/wudi/Downloads/HuaZai/测试3.docx";
+		String outPath = "/Users/wudi/Downloads/HuaZai/测试gl.docx";
 
 		try {
 			FileOutputStream fileOutputStream = new FileOutputStream(outPath);
 			document.write(fileOutputStream);
 
-			System.out.println("fileOutputStream = " + fileOutputStream);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+
+	}
+
+
+	@Test
+	public void insertPictureTest2() {
+
+
+		XWPFDocument document = new XWPFDocument();
+
+		String imgUrl = "https://mhp-test.oss-cn-qingdao.aliyuncs.com/images/test/ms/file/notFilename/20230920161635/20230920161634_3_.png";
+
+		InputStream inputStream = null;
+		try {
+			URL url = new URL(imgUrl);
+			URLConnection urlConn = url.openConnection();
+			inputStream = urlConn.getInputStream();
+
+			if (inputStream != null) {
+				WordUtils.insertPicture(document, inputStream,14.6,12,ParagraphAlignment.CENTER);
+
+				String outPath = "/Users/wudi/Downloads/HuaZai/测试cc.docx";
+				FileOutputStream fileOutputStream = new FileOutputStream(outPath);
+				document.write(fileOutputStream);
+			}
+
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
